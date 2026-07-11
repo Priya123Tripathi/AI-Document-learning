9ca69cedc8117b550610d4e5858b591cf58d1ccf
Priya123Tripathi <priyakhejuri01@gmail.com>
2026-07-11 22:49:22 +0530

    Update QuizScore.jsx

--- frontend/src/pages/QuizScore.jsx
+++ frontend/src/pages/QuizScore.jsx
@@ -158,7 +158,7 @@ export default function QuizScore() {
           {/* Back */}
           <div className="text-center">
             <button
-              onClick={() => navigate(-2)}
+               onClick={() => navigate(`/documents/${quiz.documentId}`, { replace: true })}
               className="w-full md:w-auto bg-orange-600 text-white px-6 py-2 rounded-lg hover:bg-orange-700 transition"
             >
               Go Back
